107e2a407b3ee9c2c1d5da73ef2bd29a309afd2b
adi1eg <aditi.egnify@gmail.com>
2020-05-26 22:50:11 +0530

    experimenting

---
+++ azkaban-common/src/test/java/azkaban/server/session/CacheTest.java
@@ -0,0 +1,58 @@
+package azkaban.server.session;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import azkaban.flow.Flow;
+import azkaban.project.DirectoryFlowLoader;
+import azkaban.project.Project;
+import azkaban.test.executions.ExecutionsTestUtil;
+import azkaban.utils.Props;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CacheTest {
+
+  final long shortTTL = 100L;
+  final long longTTL = 10 * 10 * 100L;
+  //  private Cache<String, Session> cache;
+  private Project project;
+  private Props props;
+
+  @Before
+
+  public void setUp() throws Exception {
+    this.project = new Project(2, "test");
+    this.props = new Props();
+    final DirectoryFlowLoader loader = new DirectoryFlowLoader(this.props);
+    loader.loadProjectFlow(this.project, ExecutionsTestUtil.getFlowDir("embedded"));
+    Assert.assertEquals(0, loader.getErrors().size());
+    this.project.setFlows(loader.getFlowMap());
+    this.project.setVersion(123);
+  }
+
+  @Test
+  public void testCache() throws Exception {
+    final List<Flow> flows = this.project.getFlows();
+    System.out.println(this.project.getName());
+    final Cache<Integer, Project> cache = CacheBuilder.newBuilder()
+        .expireAfterAccess(this.longTTL, SECONDS)
+        .maximumWeight(1100)
+        .weigher((key, project) -> 100).build();
+    for (int i = 0; i < 12; i++) {
+      final Project project = new Project(i, "test");
+      cache.put(i, project);
+//
+      cache.getIfPresent(0);
+      System.out.println(cache.asMap().keySet());
+
+    }
+    System.out.println(cache.getIfPresent(0));
+//    final byte[] data = SerializationUtils.serialize((Serializable) this.project);
+  }
+}
+//
